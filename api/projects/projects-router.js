b4b94f9b4f639604db1e2d773baa10455b72f50b
Madcodes831 <r.medina2119@gmail.com>
2023-11-13 19:02:11 -0800

    server and depencies set up

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,23 @@
-// Write your "projects" router here!
+const express = require('express');
+const Projects = require('./projects-model');
+
+
+const router = express.Router();
+
+router.get('/', async (req,res, next) => {
+    const projects= await Projects.get()
+    try {
+        res.status(200).json(projects)
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.use((err, req, res, next) => { // eslint-disable-line
+    res.status(500).json({
+      message: err.message,
+      customMessage: "error within the projects router"
+    });
+  });
+
+  module.exports = router;
